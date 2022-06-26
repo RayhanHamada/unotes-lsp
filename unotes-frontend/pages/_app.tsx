490f939e85c990ad42bd16d2e1158391ced20e86
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-06-27 00:21:26 +0700

    feat: add home

--- unotes-frontend/pages/_app.tsx
+++ unotes-frontend/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css';
+import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'dark',
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
 export default MyApp;
--- unotes-frontend/pages/index.tsx
+++ unotes-frontend/pages/index.tsx
@@ -1,7 +1,74 @@
+import {
+  AppShell,
+  Button,
+  Container,
+  Header,
+  Navbar,
+  Space,
+  Text,
+} from '@mantine/core';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <AppShell
+        padding="md"
+        header={
+          <Header
+            height={60}
+            p="xs"
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+            fixed
+          >
+            <Text>
+              <b>U-Notes</b>
+            </Text>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <Button variant="outline" size="xs">
+                Login
+              </Button>
+              <Space w="md" />
+              <Button variant="outline" size="xs">
+                Daftar
+              </Button>
+            </div>
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+            height: '100vh',
+            paddingTop: '80px',
+          },
+        })}
+      >
+        <Container
+          style={{
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Text align="center" size="lg">
+            <h2>"Making Notes Never Been This Easier"</h2>
+          </Text>
+          <Text align="center">
+            U-Notes enables U to have your own simple notes that you can use
+            later
+          </Text>
+          <Text align="center">It's Completely Free</Text>
+        </Container>
+      </AppShell>
+    </>
+  );
 };
 
 export default Home;
